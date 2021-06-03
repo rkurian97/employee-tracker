786ad026c54e669ac489ab7a5eae3032ed001e5c
rkurian <kurian.rohit1229@gmail.com>
2021-06-03 17:01:33 -0500

    added connection file, db, schema, and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,28 @@
+INSERT INTO department
+    (name)
+VALUES
+    ('Engineering'),
+    ('Legal'),
+    ('Sales'),
+    ('Finance')
+
+INSERT INTO role
+    (title, salary, department_id)
+VALUES 
+    ('Sales Lead', 100000.0, 3),
+    ('Salesperson', 80000.0, 3),
+    ('Lead Engineer', 150000.0, 1),
+    ('Software Engineer', 120000.0, 1),
+    ('Accountant', 125000.0, 4),
+    ('Legal Term Lead', 250000.0, 2),
+    ('Lawyer', 190000.0, 2)
+
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
+VALUES
+    ('Rohith', 'Kurian', 3)
+    ('Sarah', 'Jayne', 1)
+    ('Bruce', 'Wayne', 6)
+    ('Rohith', 'Kurian', 2, 1)
+    ('Rohith', 'Kurian', 4, 2)
+    ('Rohith', 'Kurian', 7, 3)
